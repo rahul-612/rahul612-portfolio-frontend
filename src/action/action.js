e7a627beea546b2ae1f2453802f9b83f2a78d34c
Rahul <rk785164@gmail.com>
2022-09-25 18:10:14 +0530

    some bugs fixed

--- src/action/action.js
+++ src/action/action.js
@@ -4,7 +4,7 @@ export const sendMessage=(name,email,msg)=>async(dispatch)=>{
     try{
         dispatch({type:"sendMsgRequest"});
 
-        const {data} =await axios.post("/sendMsg",{name,email,msg},{
+        const {data} =await axios.post("https://rahul612-portfolio-api.onrender.com/sendMsg",{name,email,msg},{
             headers:{
                 "Content-Type":"application/json"
             }
